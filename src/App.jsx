562fb96397defbdd67bcc3246f2744a3a7a1f836
Cesarjes99 <cesarjes1999@gmail.com>
2025-08-22 14:39:34 -0700

    Chore: Corrected typo on Rin

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>rin &lt;3 </h1>
+      <h1>Rin &lt;3 </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
