8f7e2b01d3914380e1fdf81c0e6f54e84156d1dc
Cesarjes99 <cesarjes1999@gmail.com>
2025-08-26 13:22:27 -0700

    Feat: added landing page

--- src/views/LandingPage/LandingPage.jsx
+++ src/views/LandingPage/LandingPage.jsx
@@ -1,10 +1,47 @@
 import './LandingPage.css'
+import {Box, Button} from '@mui/material'
 
 function LandingPage (){
   return (
-    <section>
-      <img className="menu-image" alt="Menu Image" src="https://d3kjluh73b9h9o.cloudfront.net/original/3X/5/e/5ee4d3103d54f8edf6d9b1aa211042445b7b6c9e.jpeg" />
-    </section>
+    <Box
+      sx={{
+        width: 944,
+        height: 708,
+        border: '1px solid #ccc',
+        backgroundImage: `url("https://d3kjluh73b9h9o.cloudfront.net/original/3X/5/e/5ee4d3103d54f8edf6d9b1aa211042445b7b6c9e.jpeg")`,
+        backgroundSize: 'cover', 
+        backgroundPosition: 'center', 
+      }}
+    >
+      <Box
+        sx={{display: 'flex',
+              flexDirection:'column',
+              alignItems:'center'
+        }}>
+      <img className="logo-image" alt="Logo image" src="https://img.itch.zone/aW1nLzI5MDAzOTMucG5n/original/oWgoJ5.png" />
+
+      <Box sx={{display:"flex",
+                gap:"80px",
+                marginTop:'30px'
+      }}>
+      <Button variant="contained" 
+              sx={{bgcolor:'#EE3B40',
+                    padding:'15px 60px',
+                    borderRadius:'15px'
+              }}>
+        Sign up
+      </Button>
+      <Button variant="contained"
+              sx={{bgcolor:'#2C2C2C',
+                  padding:'15px 60px',
+                  borderRadius:'15px'
+              }}>
+        Log in
+      </Button>
+      </Box>
+
+      </Box>
+    </Box>
   )
 }
 
