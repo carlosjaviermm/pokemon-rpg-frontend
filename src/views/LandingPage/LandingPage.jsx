36e527b9d4a2496801deab304360da7390a58268
Cesarjes99 <cesarjes1999@gmail.com>
2025-08-26 12:29:25 -0700

    Feat: Created landing page files JSX and CSS

---
+++ src/views/LandingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+import './LandingPage.css'
+
+function LandingPage (){
+  return (
+    <section>
+      <img className="menu-image" alt="Menu Image" src="https://d3kjluh73b9h9o.cloudfront.net/original/3X/5/e/5ee4d3103d54f8edf6d9b1aa211042445b7b6c9e.jpeg" />
+    </section>
+  )
+}
+
+export default LandingPage
